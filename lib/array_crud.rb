b30e01c07c433257f513eab0b688c54a30db440c
Patrick Cremin <patrickfxcremin@gmail.com>
2020-06-03 19:03:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -43,6 +43,5 @@ end
 
 def update_element_from_index(array, index_number, element)
 update_element_from_index = ["smoke" , "beer" , "moose" , "cat" , "bong"]
-update_element_from_index[4]
-=> "totally"
+update_element_from_index[4] = "totally"
 end
