0420fc307f0b4a359b57db1be460df2eec4bb00d
Patrick Cremin <patrickfxcremin@gmail.com>
2020-06-03 18:56:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -37,7 +37,8 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  retrieve_last_element_from_array = ["sick" , "arrays!"]
+  retrieve_last_element_from_array.last
 end
 
 def update_element_from_index(array, index_number, element)
