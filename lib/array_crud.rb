dd705454af6a4ef1c70c9f3d14f65a73ecbba71d
Patrick Cremin <patrickfxcremin@gmail.com>
2020-06-03 18:29:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   ["Sick Dude", 1]
-  add_element_to_end_of_array << "Hell Ya"
+  add_element_to_end_of_array << 3
 end
 
 def add_element_to_start_of_array(array, element)
